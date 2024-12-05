eeb9a99378219cfed31972f8473b35a2d91b3685
Mrs. Kikta <melissakikta@gmail.com>
2024-12-04 22:10:24 -0500

    html routes.ts

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -1,6 +1,7 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { Router } from 'express';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const router = Router();
@@ -8,9 +9,19 @@ const router = Router();
 
 // Done: Define route to serve index.html
 router.get('/', (_, res) => {
-  const filePath = path.join(__dirname, '../client/public', 'index.html');
-  res.sendFile(filePath);
+  const indexPath = path.join(__dirname, '../../public/index.html');
+
+
+  res.sendFile(indexPath, (err) => {
+    if (err) {
+      console.error('Error serving index.html:', err);
+      res.status(500).send('Internal Server Error');  
+    }
+  });
 });
 
 
 export default router;
+
+
+//const indexPath = path.join(__dirname, '../client/public', 'index.html'
